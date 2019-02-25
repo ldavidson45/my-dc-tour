29742d2abe944b3df6dbb04cc0958e29630b59b4
Liz Davidson <lizdavidson45@gmail.com>
2019-02-25 18:50:45 -0500

    set up index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.use(bodyParser.urlencoded( { extended: true}));
+app.set("view engine", "hbs");
+app.use(methodOverride("_method"));
+
+app.listen(3000, () => console.log("server is listening on port 3000"))
