15304a1f26a381870174c5262056d3db4e18bf58
Liz Davidson <lizdavidson45@gmail.com>
2019-02-26 13:08:17 -0500

    build out model for Tours

--- models/Tour.js
+++ models/Tour.js
@@ -1,5 +1,11 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
+const TourSchema = new Schema ({
+    title: String,
+    description: String,
+    landmarks: [{type: Schema.ObjectID, ref: 'Landmark'}],
+})
+
 
 module.exports = mongoose.model("Tour", TourSchema);
