dc3fb4de91d1280c6c8ec30202a78790e9793719
Liz Davidson <lizdavidson45@gmail.com>
2019-02-27 15:05:47 -0500

    add full CRUD to landmark resource; add edit view and modify show view for landmark

--- controllers/landmark.js
+++ controllers/landmark.js
@@ -14,12 +14,33 @@ module.exports = {
             res.redirect(`/landmark/${landmark._id}`);
         })
     },
-    show: function (req,res){
+        show: function (req,res){
+            Landmark.findById(req.params.id)
+            .then(landmark => {
+                res.render('landmark/show', { landmark })
+
+            })
+    },
+    edit: function(req, res) {
         Landmark.findById(req.params.id)
         .then(landmark => {
-            res.render('landmark/show', { landmark })
-
+            res.render('landmark/edit', { landmark });
         })
+    },
+    update: function(req, res) {
+        const { title, neighborhood, description } = req.body;
+        Landmark.findByIdAndUpdate(req.params.id, {
+            title,
+            description,
+            neighborhood
+        }).then(landmark => {
+            res.redirect(`/landmark/${landmark._id}`)
+        })
+
+    },
+    delete: function (req, res) {
+        Landmark.findByIdAndRemove(req.params.id)
+        .then(res.redirect('/application'))
     }
     
 }
--- routes/landmark.js
+++ routes/landmark.js
@@ -4,6 +4,9 @@ const landmarkController = require("../controllers/landmark");
 
 router.get('/new', landmarkController.new);
 router.post('/', landmarkController.create);
-router.get('/:id', landmarkController.show)
+router.get('/:id', landmarkController.show);
+router.get('/:id/edit', landmarkController.edit);
+router.put('/:id', landmarkController.update);
+router.delete('/:id', landmarkController.delete)
 
 module.exports = router;
