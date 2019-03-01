5bb1c5fd75a308476330277a73fdecd97b1e5034
Liz Davidson <lizdavidson45@gmail.com>
2019-03-01 13:19:48 -0500

    edit title and nav bar

--- db/connection.js
+++ db/connection.js
@@ -5,5 +5,5 @@ module.exports = mongoose;
 if (process.env.NODE_ENV == "production") {
     mongoose.connect (process.env.DB_URL)
 } else {
-    mongoose.connect("mongodb://localhost/history-facts-app")
+    mongoose.connect("mongodb://localhost/history-facts-app", {useNewUrlParser: true})
 }
