309bb1dae654c1d8fb8cf9f3d5bacc10985603e7
Liz Davidson <lizdavidson45@gmail.com>
2019-02-28 23:02:13 -0500

    update login form

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.use('/user', require('./user.js'));
 router.use('/landmark', require('./landmark.js'));
 router.use('/tour', require('./tour.js'));
 
+router.get('/', function (req, res) {
+    res.render('user/login')
+});
 
 module.exports = router;
