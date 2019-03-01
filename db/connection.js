ddc2a2041e56ce8b297bc85c109439057d27d372
Liz Davidson <lizdavidson45@gmail.com>
2019-03-01 12:58:42 -0500

    set up heroku configuration

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/history-facts-app");
 mongoose.Promise = Promise;
 module.exports = mongoose;
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect (process.env.DB_URL)
+} else {
+    mongoose.connect("mongodb://localhost/history-facts-app")
+}
--- index.js
+++ index.js
@@ -16,4 +16,8 @@ app.use(methodOverride("_method"));
 app.use(express.static('public'));
 app.use(require('./routes/index.js'))
 
-app.listen(3000, () => console.log("server is listening on port 3000"))
+app.set('port', process.env.PORT || 3001)
+
+app.listen(app.get('port'), () => {
+    console.log(`✅ PORT: ${app.get('port')} 🌟`)
+})
