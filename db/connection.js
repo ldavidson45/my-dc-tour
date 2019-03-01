a0a2934828d846c4ccd68b87595fc0db23cd3456
Liz Davidson <lizdavidson45@gmail.com>
2019-03-01 16:06:46 -0500

    update db connection bug

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
-    mongoose.connect (process.env.DB_URL)
+    mongoose.connect (process.env.DB_URL,  { useNewUrlParser: true })
 } else {
     mongoose.connect("mongodb://localhost/history-facts-app", { useNewUrlParser: true })
 }
