21fafa7a4a3f7a41176143fa7ae8b1bb68e2e72a
Liz Davidson <lizdavidson45@gmail.com>
2019-03-01 15:59:38 -0500

    fix db/connection mongoose connection

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
-module.exports = mongoose;
 
 if (process.env.NODE_ENV == "production") {
     mongoose.connect (process.env.DB_URL)
 } else {
-    mongoose.connect("mongodb://localhost/history-facts-app", {useNewUrlParser: true})
+    mongoose.connect("mongodb://localhost/history-facts-app", { useNewUrlParser: true })
 }
+
+module.exports = mongoose;
