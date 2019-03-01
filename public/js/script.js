ee56160ab26e0d1474da3133d92ae81b71b8ea23
Liz Davidson <lizdavidson45@gmail.com>
2019-03-01 11:26:34 -0500

    implement google maps api

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
+var key = AIzaSyDqrPjYSAGEIGk8LTcKv1ICpaLUmP-tQU4
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.89511, lng: -77.03637},
-    zoom: 12
+    zoom: 13
   });
 
 }
